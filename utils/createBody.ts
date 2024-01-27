0caca7721b86b50ff25b216684c130a6b5c8232e
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2024-01-27 12:15:47 +0900

    fix

--- utils/createBody.ts
+++ utils/createBody.ts
@@ -1,5 +1,5 @@
 import { Buffer } from "npm:buffer";
 
 export function createBody(): string {
-    return Buffer.alloc(2 * 1024 * 1024 * 45).toString("UTF-8"); 
+    return Buffer.alloc(2 * 1024 * 1024 * 20).toString("UTF-8"); 
 }
